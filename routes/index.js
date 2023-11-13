ec7bf5e9cdc8a605314a3dbb774782a2ca088455
phburns <phburns@gus.pittstate.edu>
2023-11-12 22:39:51 -0600

    a lot of stuff

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,47 @@
 // routes/index.js
 var express = require('express');
 var router = express.Router();
+var axios = require('axios'); // Add this line to import axios
 
 router.get('/', function(req, res, next) {
   res.render('login');
 });
 
 router.post('/dashboard', function(req, res, next) {
-  res.redirect('/dashboard');
+  // Fetch data from JSONPlaceholder (replace with your desired endpoint)
+  axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(response => {
+      res.render('dashboard', { title: 'Dashboard', todos: response.data });
+    })
+    .catch(error => {
+      console.error(error);
+      res.render('error');
+    });
 });
 
 router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard');
+  // Fetch data from JSONPlaceholder (replace with your desired endpoint)
+  axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(response => {
+      res.render('dashboard', { title: 'Dashboard', todos: response.data });
+    })
+    .catch(error => {
+      console.error(error);
+      res.render('error');
+    });
 });
 
 router.get('/profile', function(req, res, next) {
-  res.render('profile');
+  // Fake user data
+  var user = {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    username: 'johndoe',
+    phone: '1234567890',
+    website: 'www.example.com'
+  };
+
+  res.render('profile', { user: user });
 });
 
 router.get('/logout', function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+router.get('/', function(req, res, next) {
+    // Fetch data from JSONPlaceholder (replace with your desired endpoint)
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+        .then(response => {
+            res.render('profile', { title: 'Profile', todos: response.data });
+        })
+        .catch(error => {
+            console.error(error);
+            res.render('error');
+        });
+});
+
+module.exports = router;
